660f2b180d5c3d8d4ce5aa80000bedc4dd96ef78
unknown <demoshu2022@gmail.com>
2026-02-14 14:47:18 -0500

    pom issue ansd main java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
-package src;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is a simple Java project.");
+        System.out.println("Hello from Maven + Jenkins!");
     }
 }
+
+// package src;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//         System.out.println("This is a simple Java project.");
+//     }
+// }
